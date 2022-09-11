a4245e989c25f8c78ee2973fb19cc6dc5dde55b5
RahulDubey391 <100185371+RahulDubey391@users.noreply.github.com>
2022-09-11 13:29:40 +0530

    Update GCSOp.py

--- comStore/storages/gcs/GCSOp.py
+++ comStore/storages/gcs/GCSOp.py
@@ -4,16 +4,22 @@ from google.cloud import storage
 class GCSOperator(object):
 
     def __init__(self):
-        pass
+        self.client = storage.Client()
 
-    def put(self,path: str) -> None:
-        pass
+    def put(self,bucket_name: str,blob_name: str,data: str) -> None:
+        bucket = self.client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_string(data)
 
-    def get(self,path: str) -> None:
-        pass
+    def get(self,bucket_name: str,blob_name: str):
+        bucket = self.client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        return blob
 
-    def list(self,path: str) -> None:
-        pass
+    def list(self,bucket_name: str) -> list:
+        bucket = self.client.get_bucket(bucket_name)
+        blobs = bucket.list_blobs()
+        return blobs
 
     def move(self,src_path: str, dest_path: str) -> None:
         pass
